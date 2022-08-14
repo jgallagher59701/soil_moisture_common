a4556883eb80b7f9fb446143ff8f1154c3212279
James Gallagher <jgallagher@opendap.org>
2022-08-14 11:04:07 -0600

    Added new, smaller, data packet

--- data_packet.h
+++ data_packet.h
@@ -6,7 +6,7 @@
 #include <Arduino.h>
 
 /// Size of the data packet in bytes
-#define DATA_PACKET_SIZE sizeof(packet_t)
+#define DATA_PACKET_SIZE sizeof(packet_t)   // 19 bytes
 
 struct packet_t {
     uint32_t message;
@@ -28,5 +28,29 @@ void parse_data_packet(const packet_t *data, uint8_t *node, uint32_t *message, u
                        uint16_t *last_tx_duration, int16_t *temp, uint16_t *humidity, uint8_t *status);
 
 char *data_packet_to_string(const packet_t *data, bool pretty = false);
+
+#if 0   /// Not yet... jhrg 2/5/22
+#define DATA_PACKET_2_ SIZE sizeof(packet_2_t)  // 11 bytes
+
+struct packet_2_t {
+    uint16_t message;       // 65536 message maximum
+    uint32_t time;
+    int16_t temp;
+    uint8_t humidity;
+    uint8_t battery;        // really only needs 4 bits; y = 0.1x + 2.7; 0 <= x <= 10
+    uint8_t status;         // only using 4 bits
+    // uint8_t node; Not needed since the code uses 'reliable datagrams' and that includes the node number
+};
+
+void build_data_packet2(packet_2_t *data, const uint16_t message, const uint32_t time,
+                        const uint8_t battery, const int16_t temp, const uint8_t humidity, 
+                        const uint8_t status);
+
+void parse_data_packet2(const packet_2_t *data, uint16_t *message, uint32_t *time,
+                        uint8_t *battery, int16_t *temp, uint8_t *humidity, 
+                        uint8_t *status);
+
+char *data_packet_to_string2(const packet_2_t *data, bool pretty = false);
+#endif
 
 #endif
